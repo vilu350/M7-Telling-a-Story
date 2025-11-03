d05bccfd64962ac1c5f68ebc969d19931e9d3028
vilu350 <vilu@s.sfusd.edu>
2025-11-03 15:49:19 -0800

    worked more on M7L5

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,1 +1,10 @@
 extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+var dialogue_items : Array[String] = [
+	"beep",
+	"blorp",
+	"vorp",
+]
