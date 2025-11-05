aaa9ebe5e15ced09666530b499f875a522d25294
vilu350 <vilu@s.sfusd.edu>
2025-11-05 13:51:11 -0800

    worked on M7L6

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 
 var dialogue_items : Array[String] = [
 	"beep",
@@ -19,6 +20,10 @@ func _ready() -> void:
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 0.5
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 
 func advance() -> void:
 	current_item_index += 1
