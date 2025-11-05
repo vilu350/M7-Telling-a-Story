12d28cd9e485606a50bfd641c988b02473d04daa
vilu350 <vilu@s.sfusd.edu>
2025-11-05 13:25:56 -0800

    finished M7L5

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -7,4 +7,22 @@ var dialogue_items : Array[String] = [
 	"beep",
 	"blorp",
 	"vorp",
+	"merp",
 ]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
