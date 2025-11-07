14d566faa99a0c70e29e97db7809ba8c1936517f
vilu350 <vilu@s.sfusd.edu>
2025-11-07 15:49:11 -0800

    Worked on M7L8

--- M07.telling_a_story_workbook/lessons/expressions.gd
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -1,5 +1,5 @@
 extends Control
-
+#left off at Using Lambda functions for one-off functions
 @onready var body: TextureRect = %Body
 @onready var expression: TextureRect = %Expression
 @onready var button_sophia: Button = %ButtonSophia
@@ -7,3 +7,18 @@ extends Control
 @onready var button_regular: Button = %ButtonRegular
 @onready var button_sad: Button = %ButtonSad
 @onready var button_happy: Button = %ButtonHappy
+
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png"),
+}
+
+var expressions := {
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+	"happy": preload("res://assets/emotion_happy.png"),
+}
+
+func _ready() -> void:
+	body.texture = bodies["pink"]
+	expressions.texture = expressions["happy"]
