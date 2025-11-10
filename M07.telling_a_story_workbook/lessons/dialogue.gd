4fb526fef15e740a7d5280798c1e120936146b54
vilu350 <vilu@s.sfusd.edu>
2025-11-09 22:37:28 -0800

    Worked on M7L10 (Broken)

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -3,12 +3,32 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-var dialogue_items : Array[String] = [
-	"merp",
-	"blorbo borp vorp meep beep",
-	"blop gop gobble bop",
-	"beep",
+var expressions := {
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+	"happy": preload("res://assets/emotion_happy.png"),
+}
+
+var dialogue_items: Array[Dictionary] = [
+	{
+	"expresion": expressions["regular"],
+	"text": "merp",
+	},
+	{
+	"expresion": expressions["happy"],
+	"text": "blorbo borp vorp meep beep",
+	},
+	{
+	"expresion": expressions["sad"],
+	"text": "blop gop gobble bop",
+	},
+	{
+	"expresion": expressions["regular"],
+	"text": "beep",
+	},
 ]
 
 var current_item_index := 0
@@ -19,10 +39,11 @@ func _ready() -> void:
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 0.6
+	var text_appearing_duration: float = 0.6
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
